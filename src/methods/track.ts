d9513340c5733d5716fa5725c259885d4352e208
Joyce Huang <huangjoyce3@gmail.com>
2022-10-28 11:03:04 -0500

    event properties

--- src/methods/track.ts
+++ src/methods/track.ts
@@ -1,10 +1,23 @@
-import Sprig from 'react-native-userleap';
-import type { TrackEventType } from '@segment/analytics-react-native';
-const SIGN_OUT_EVENT = 'Signed Out';
+import Sprig from "react-native-userleap";
+import type { TrackEventType } from "@segment/analytics-react-native";
+const SIGN_OUT_EVENT = "Signed Out";
 export default (event: TrackEventType) => {
   if (event.event === SIGN_OUT_EVENT) {
     Sprig.logout();
   } else {
-    Sprig.trackIdentifyAndPresent(event.event, event.userId, event.anonymousId);
+    if (Object.keys(event.properties).length)
+      Sprig.trackWithProperties(
+        event.event,
+        event.userId,
+        event.anonymousId,
+        event.properties,
+        Sprig.presentSurvey
+      );
+    else
+      Sprig.trackIdentifyAndPresent(
+        event.event,
+        event.userId,
+        event.anonymousId
+      );
   }
 };
